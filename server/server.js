25d087e006c6891cdd3ff13d53040fd6c251298f
Rune Tonstad <runerten@gmail.com>
2023-08-10 16:34:05 +0200

    Step 9 - Listening on server, added dotenv etc. Added root package.json to github. Placing build etc for server, client and root. Added client dev scripts + some more root scripts

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-const server = app.listen(3000, () => {
-    console.log('Started server on http://localhost:3000:${server.address().port}');});
-});
+
+const server = app.listen(process.env.port || 3000,
+    () => {console.log(`Started server on http://localhost:3000:${server.address().port}`);
+
+    });
