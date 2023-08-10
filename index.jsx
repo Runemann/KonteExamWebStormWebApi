ab6a485be20914ed6bafec12a5b7af1ee3af6852
Rune Tonstad <runerten@gmail.com>
2023-08-10 11:46:28 +0200

    Step 1 - Start up of project with index.html, index.jsx, react etc dependencies, parcel and a read me file.

---
+++ index.jsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("app"));
+
+function Home(){
+    return <div>
+        <h1> WorkSpace </h1>
+        <ul>
+            <li><Link to="/home"></Link></li>
+            <li><Link to="/staff"></Link></li>
+        </ul>
+    </div>
+}
+
+
+function Application(){
+    return <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home/>}></Route>
+        </Routes>
+    </BrowserRouter>
+}
+
+root.render(
+    <Application/>
+);
